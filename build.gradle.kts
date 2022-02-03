27bd274f0126cbbd9201b0233b7279d4b0309e68
David Winnicki <david.winnicki@codeabsolute.tech>
2022-02-03 15:13:12 -0500

    CI changes - Updated gradle files - Updated GitHub Actions - Changed apk to aab

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,9 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.1.0-rc01")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("com.google.gms:google-services:4.3.10")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.0")
     }
 }
 
@@ -15,6 +16,20 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+    subprojects {
+        afterEvaluate {
+            project.extensions.findByType<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension>()?.let { ext ->
+                ext.sourceSets.removeAll { sourceSet ->
+                    setOf(
+                        "androidAndroidTestRelease",
+                        "androidTestFixtures",
+                        "androidTestFixturesDebug",
+                        "androidTestFixturesRelease",
+                    ).contains(sourceSet.name)
+                }
+            }
+        }
     }
 }
 
